5783963114afdb07faf8a42eb68023decbd03d7d
voytus <voytus@protonmail.com>
2022-06-06 18:32:30 +0100

    WTM: added stuff

--- introductory1/format-coloured.js
+++ introductory1/format-coloured.js
@@ -1,8 +1,8 @@
-// Coloured text output
-const PREFIX = '\x1b'; // for indicating we are using an ANSI colour command
-const RESET = PREFIX + "[0m"; // returns us to the default settings
+const PREFIX = '\x1b';
+
 const username = "Bob";
-// Foregrounds
+
+const RESET = PREFIX + "[0m";
 const FG_BLACK = PREFIX + "[30m";
 const FG_RED = PREFIX + "[31m";
 const FG_GREEN = PREFIX + "[32m";
@@ -22,12 +22,9 @@ const BG_MAGENTA = PREFIX + "[45m";
 const BG_CYAN = PREFIX + "[46m";
 const BG_WHITE = PREFIX + "[47m";
 
-// interpolated strings (ES2015 template literals)
-// - strings with backticks
-// - we can put expressions inside ${} in a string to insert the values
-console.log(`Hello ${FG_YELLOW}BOB${RESET}.`);
 
-console.log(`Hello ${BG_CYAN}${FG_YELLOW}"%s"${RESET}.`);
+//console.log(`"${FG_GREEN}${BG_WHITE}Hello${RESET} %s, username"`);
 
+//console.log("`${FG_GREEN}${BG_WHITE}Hello${RESET} %s, username`");
 
-//console.log("Hello " + FG_YELLOW + %s + RESET + ".", username);
+console.log("${FG_GREEN}${BG_WHITE}Hello${RESET} %s, username");
--- introductory1/format-specifiers.js
+++ introductory1/format-specifiers.js
@@ -2,11 +2,20 @@
 // - we can use these to insert values into text
 // - %s for strings
 // - %d for numbers
-// console.log(formatString, args...);
+
+//                           args - passing a VARIABLE NUMBER OF ARGUMENTS - depends of a count of %s (FORMAT SPEC)
+// console.log(formatString, args...);  
+//            FORMAT STRINGS AS A CORE OF STRING ORDER which prints into a console
 const username = "Bob";
 console.log("Hello %s", username);
-console.log("Hello %s, we can insert multiple strings: %s", username, "inserted");
 
+console.log("Hello %s, we can insert multiple strings: %s", username, "inserted");
+//                 %s-PLACE HOLDER                     %s- Second place holder 
 console.log("We can also insert numbers: %d", 32 + 5 / 4);
 
+
+// // string without a specyfication of any PLACE HOLDER
 console.log("We can insert args without specifiers as well:", username);
+
+
+console.log("We can insert args without specifiers as well:", 1 + 10 + 100 + 1000);
