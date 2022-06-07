22306fd92e23a1804c77338f7789dd0cbcc84f31
voytus <voytus@protonmail.com>
2022-06-07 13:59:18 +0100

    WTM: added notes to modules2

--- introductory1/modules/index.js
+++ introductory1/modules/index.js
@@ -4,6 +4,10 @@ const Animal = require('./Animal'); // use the 'require' keyword to import a mod
 // if only importing a function from a module, we must specify curly braces  around it
 const { isEven } = require('./helpers');
 // CURLY BRACKETS ARROUND
+// ALTERNATIVE NOT TO USE A CURLY BRACKETS
+// simmilar to static import in Java
+// const isEven = require('./helpers');
+// console.log(isEven.isEven(101));
 
 
 const helpers = require('./helpers');
@@ -16,3 +20,4 @@ console.log(isEven(30));
 
 // CommonJS modules are part of Node js, they are for modularising our code
 // - i.e., separating it into separate files
+
